dabc87ca2eb2d1c187a8ecd76245b8fd7ad3aa10
ledhieus <ledan0602@gmail.com>
2024-08-12 20:51:28 +0700

    create project ts

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express, {Express} from "express"
+import dotenv from "dotenv"
+import * as database from "./config/database"
+import mainV1Routes from "./api/v1/routes/index.route"
+
+dotenv.config()
+database.connect()
+
+const app: Express = express()
+const port: number | string = process.env.PORT || 3000
+
+mainV1Routes(app)
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`)
+})
